20d57399f01dbdc8409bf575ee6648467ffeeff4
Peter Olu <32315813+pemeraldy@users.noreply.github.com>
2022-08-10 03:29:06 +0100

    fixing type error

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -48,13 +48,17 @@ class Graphql {
   public params(variablesData: unknown): AxiosPromise<void> {
     return axios
       .post(
-        "/graphql/dashboard",
+        "/graphql",
         {
           query: query[this.queryData],
           variables: variablesData,
         },
         {
           cancelToken: cancelToken(),
+          headers: {
+            Authorization:
+              "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJkZXZlbG9wZXJfaWQiOiJzeXN0ZW0iLCJjcmVhdGVkX2F0IjoxNjYwMDk3MzE1NDAzLCJleHBpcmVzX2F0IjoxNjkxNjMzMzE1NDAzLCJpYXQiOjE2NjAwOTczMTV9.PvPXbBWsqJH0I1Bdh0Tl61h1IRofrukdnKVFad92yRWR6bLT1mxQR6WeXZj_ZZnaakrgzY_x3ojJg-dgAcINU4Q8iyDqDShM8fTlhT1WkzfZAFNBAFyGP6bMzTycAH2hyCRRi6rUrK-16gSL5cSopUtya0SVWKr_aSh96lEQCFUDKg9fLwNu3vGTXhNbhpnoshABwOXVMnwvet73R8BU8gIeKjI953xyN9Xqp7mV4wNZGC3ZjzrLQ0kVGY3AYYSoeqHzlYwKA-S5yuGlsnGyjyJqpBhOuYCfMWNmE_WJ_km2ikuBIkR9ZwLZ5E1hBBklKOG-paxQyQ13HnzIhGe5IgA5CGZ3P-6wH6ckI1ZUdSo8Cm4xUEXekwcLX2pHluUWX0qkHKDAwB3tgvhbN5_zTJPWRr1QFmfhckt-7YV8YieDhGT4aJdav78KhwKEb5esNumYYNrUFYUmhHDqniR3nZAjhDwl0zoK3eVdxU3SZq2py5SUPxSUFnIhJVS6kapHdwrPS8fFZxMHt0RwyxjkOsQgsvfVcwc8cbCx6kGaZdFawyKe9PtAe6b58zrO-mzuCm7OzA0q_pT0qZsRjh7ybDF9P1Qy6m_jeG6L7duI9J60iK9LpljACAG7qt5LyPT0t8Aqv-gE704Rc-dTASi3V44yroA7Br_-qIYTw5ux_QA",
+          },
         }
       )
       .then((res: AxiosResponse) => {
